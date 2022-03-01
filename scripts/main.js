48323243d7cf0b42e2623885aecb5749a10afc91
pcammayo <pcammayo03@gmail.com>
2022-03-01 12:19:46 -0800

    Finished bonus task

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ function read_display_Quote() {
         document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;
     })
 }
-
 read_display_Quote();
 
 function insertName() {
@@ -27,3 +26,88 @@ function insertName() {
     })
 }
 insertName();
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code:"hike1",
+        name: "Burnaby Lake Park Trail",    //replace with your own city?
+        city: "Burnaby",
+        province: "BC",
+        level: "easy",
+        length: "10 km",
+        details: "Elmo goes here regularly"
+    });
+    hikesRef.add({
+        code:"hike2",
+        name: "Buntzen Lake Trail Trail",    //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        length: "10.5 km",
+        details: "Elmo goes here regularly"
+    });
+    hikesRef.add({
+        code:"hike3",
+        name: "Mount Seymoure Trail",    //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        length: "8.2 km",
+        details: "Elmo goes here regularly"
+    });
+}
+
+// writeHikes();
+
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;   // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+                let newcard = cardTemplate.content.cloneNode(true);
+                var code = doc.data().code; 
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/" + code + ".jpg"; //hikes.jpg
+
+                //give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+
+displayCards("hikes");
+
+/*
+function displayHikes() {
+    firebase.auth().onAuthStateChanged(hike => {
+        if (hike) {
+            console.log(hike.uid);
+
+            currentHike = db.collection("hikes").doc(hike.uid);
+
+            currentHike.get().then(hikeDoc => {
+                var hike_Name = hikeDoc.data().name;
+                console.log(hike_name);
+                document.getElementById("card-title").innerText = hike_Name;
+            })
+        }
+    })
+}
+displayHikes();
+*/
